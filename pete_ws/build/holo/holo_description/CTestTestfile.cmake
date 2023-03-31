06c7128c07c35e539da5cceda720d9aaf1dad209
Natchanon <108419826+Natchanon9@users.noreply.github.com>
2023-03-31 09:14:46 +0000

    Add files via upload

---
+++ pete_ws/build/holo/holo_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/pete_ws/src/holo/holo_description
+# Build directory: /home/ubuntu/pete_ws/build/holo/holo_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ pete_ws/build/holo/holo_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/pete_ws/src/holo/holo_gazebo
+# Build directory: /home/ubuntu/pete_ws/build/holo/holo_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
